68cad86c457d18a05e3dfa4af8410e26e5a5e0d7
hadeelAlwajman <81421087+hadeelAlwajman@users.noreply.github.com>
2021-04-14 13:08:05 +0300

    Update ManagerOperation.java

--- ProjectHadeel/src/ManagerOperation.java
+++ ProjectHadeel/src/ManagerOperation.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  */
 public class ManagerOperation {
     Connection c;
-    int num = 0;
+    int num = 1;
     ChildData d;
     CircularLinkedList<ChildData> child = new CircularLinkedList<>();
     ArrayList<ParentData> parent = new ArrayList<>();
@@ -30,6 +30,7 @@ public class ManagerOperation {
         }
         return null;
     }
+    
 
 
     public boolean deleteChild(int childNo){
@@ -100,178 +101,6 @@ public class ManagerOperation {
 
 
 
-//    public void viewChildData(){
-//        try
-//        {
-//            Connection c=DriverManager.getConnection("jdbc:ucanaccess:E:\\Child.accdb");
-//            Statement s=c.createStatement();
-//            ResultSet rs=s.executeQuery("SELECT * FROM ChildNo");
-//            while (rs.next()){
-//                System.out.println(rs.getInt("childNo")+"  "+rs.getString("firstName")+"  "+rs.getString("fatherName")
-//                        +"  "+rs.getString("familyName")+"  "+rs.getString("sex")+"  "+rs.getString("nationality")
-//                        +"  "+rs.getString("birthDate")+"  "+rs.getString("placeBirth")+"  "+ rs.getString("address")
-//                        +"  "+rs.getString("level")+"  "+rs.getString("dateStartUpPermanence")+"  "+rs.getString("childStatus"));
-//            }
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//    public void addParent(String fatherName, String fatherNationality, String fatherJob,
-//                          String fatherWorkAddress, String fatherMobilPhone, String fatherWorkPhone,
-//                          String housePhone, String motherName, String motherNationality, String motherJob,
-//                          String motherWorkAddress, String motherMobilPhone, String motherWorkPhone,
-//                          String firstEmail, String secondEmail, ChildData childNo){
-//        parent.add(new ParentData(fatherName,  fatherNationality,  fatherJob,
-//                fatherWorkAddress,  fatherMobilPhone,  fatherWorkPhone,
-//                 housePhone, motherName,  motherNationality,  motherJob,
-//                motherWorkAddress,  motherMobilPhone,  motherWorkPhone,
-//                 firstEmail,  secondEmail,  childNo));
-//        try {
-//            Connection c=DriverManager.getConnection("jdbc:ucanaccess:E:\\Child.accdb");
-//            PreparedStatement st;
-//            st=c.prepareStatement("insert into Parent(fatherName,  fatherNationality,  fatherJob,fatherWorkAddress,  fatherMobilPhone,  fatherWorkPhone, housePhone, motherName,  motherNationality,  motherJob, motherWorkAddress,  motherMobilPhone,  motherWorkPhone," +
-//                    " firstEmail,  secondEmail,  childNo('"+fatherName+  fatherNationality+ fatherJob+fatherWorkAddress+fatherMobilPhone+  fatherWorkPhone+ housePhone+motherName+  motherNationality+motherJob+ motherWorkAddress+  motherMobilPhone+ motherWorkPhone+
-//                    firstEmail+ secondEmail+ childNo);
-//            st.execute();
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//
-//    public void searchParentData(ChildData childNo){
-//        try {
-//            Connection c=DriverManager.getConnection("jdbc:ucanaccess:E:\\Child.accdb");
-//            Statement s=c.createStatement();
-//            String statement="SELECT * FROM ChildData where childNo="+childNo;
-//            ResultSet rs=s.executeQuery(statement);
-//            while (rs.next()){
-//                System.out.println(rs.getString("fatherName")+"  "+rs.getString("fatherNationality")+"  "+rs.getString("fatherJob")+"  " +
-//                        rs.getString("fatherWorkAddress")+"  "+rs.getString("fatherMobilPhone")+"  "+rs.getString("fatherWorkPhone")+"  "+
-//                        rs.getString("housePhone")+"  "+rs.getString("motherName")+"  "+rs.getString("motherNationality")+"  "+
-//                        rs.getString("motherJob")+"  "+rs.getString("motherWorkAddress")+"  "+rs.getString("motherMobilPhone")+"  "+
-//                        rs.getString("motherWorkPhone")+"  "+rs.getString("firstEmail")+"  "+rs.getString("secondEmail")+"  "+rs.getString("childNo"));
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
-//    public void deleteParent(ChildData childNo){
-//        try{
-//            Connection c=DriverManager.getConnection("jdbc:ucanaccess:E:\\Child.accdb");
-//            Statement st=c.createStatement();
-//            String statement="delete from parent Data where childNo= "+childNo;
-//            int i=st.executeUpdate(statement);
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//    public void viewParent(){
-//        try
-//        {
-//            Connection c=DriverManager.getConnection("jdbc:ucanaccess:E:\\Child.accdb");
-//            Statement s=c.createStatement();
-//            ResultSet rs=s.executeQuery("SELECT * FROM ChildNo");
-//            while (rs.next()) {
-//                System.out.println(rs.getString("fatherName")+"  "+rs.getString("fatherNationality")+"  "+rs.getString("fatherJob")
-//                        +"  "+rs.getString(" fatherWorkAddress")+"  "+rs.getString("fatherMobilPhone")+"  "+rs.getString("fatherWorkPhone")
-//                        +"  "+rs.getString("HousePhone")+"  "+rs.getString("MotherName")+"  "+ rs.getString("MotherNationality")
-//                        +"  "+rs.getString("MotherJob")+"  "+rs.getString("MotherWorkAddress")+"  "+rs.getString("MotherMobilPhone")
-//                        +"  "+rs.getString("MotherWorkPhone")+"  "+rs.getString("FirstEmail")+"  "+rs.getString("SecondEmail")+"  "+rs.getString("childNo"));
-//            }
-//            } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//    public void addPeopleAuthorizedToReceive(String name, int mobilePone, String relativeRelation, ChildData childNo) {
-//        try {
-//            Connection c = DriverManager.getConnection("jdbc:ucanaccess:E:\\Child.accdb");
-//            PreparedStatement s;
-//            s = c.prepareStatement("insert into PeopleAuthorizedToReceive (name,mobilePone,relativeRelation,childNo )values('"+name+mobilePone+relativeRelation+childNo);
-//            s.execute();
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
-//    public void searchPeopleAuthorizedToReceive(ChildData childNo){
-//        try {
-//            Connection c=DriverManager.getConnection("jdbc:ucanaccess:E:\\Child.accdb");
-//            Statement s=c.createStatement();
-//            String statement="SELECT * FROM ChildData where childNo="+childNo;
-//            ResultSet rs=s.executeQuery(statement);
-//            while (rs.next()){
-//                System.out.println(rs.getString("name")+"  "+rs.getInt("mobilePone")+"  "+rs.getString("relativeRelation")+"  " +
-//                       rs.getString("childNo"));
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//    public void deletePeopleAuthorizedToReceive(ChildData childNo){
-//        try{
-//            Connection c=DriverManager.getConnection("jdbc:ucanaccess:E:\\Child.accdb");
-//            Statement st=c.createStatement();
-//            String statement="delete from PeopleAuthorizedToReceive where childNo= "+childNo;
-//            int i=st.executeUpdate(statement);
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//    public void viewPeopleAuthorizedToReceive(ChildData childNo){
-//        try
-//        {
-//            Connection c=DriverManager.getConnection("jdbc:ucanaccess:E:\\Child.accdb");
-//            Statement s=c.createStatement();
-//            ResultSet rs=s.executeQuery("SELECT * FROM ChildNo");
-//            while (rs.next()) {
-//                System.out.println(rs.getString("name")+"  "+rs.getInt("mobilePone")+"  "+rs.getString("relativeRelation")
-//                        +"  "+rs.getString("childNo"));
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//    public void addDailyAttendance(ChildData childNo,String date, String day, String attendance, String departure){
-//        try {
-//            Connection c = DriverManager.getConnection("jdbc:ucanaccess:E:\\Child.accdb");
-//            PreparedStatement s;
-//            s = c.prepareStatement("insert into DailyAttendance  (name,data,day,attendance,departure )values('"+childNo+date+day+attendance+departure);
-//            s.execute();
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
-//    public void searchDailyAttendance(ChildData childNo){
-//        try {
-//            Connection c=DriverManager.getConnection("jdbc:ucanaccess:E:\\Child.accdb");
-//            Statement s=c.createStatement();
-//            String statement="SELECT * FROM DailyAttendance where childNo="+childNo;
-//            ResultSet rs=s.executeQuery(statement);
-//            while (rs.next()){
-//                System.out.println(rs.getString("childNo")+"  "+rs.getString("date")+"  "+rs.getString("day")+"  " +
-//                        rs.getString("attendance")+"  "+rs.getString("departure"));
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//    public void viewDailyAttendance(ChildData childNo){
-//        try
-//        {
-//            Connection c=DriverManager.getConnection("jdbc:ucanaccess:E:\\Child.accdb");
-//            Statement s=c.createStatement();
-//            ResultSet rs=s.executeQuery("SELECT * FROM ChildNo");
-//            while (rs.next()) {
-//                System.out.println(rs.getString("childNo")+"  "+rs.getString("date")+"  "+rs.getString("day")
-//                        +"  "+rs.getString("attendance")+"  "+rs.getString("departure"));
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
+
 
 }
